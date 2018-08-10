5dfd9bfed99ba8e0f5633409b6d05048c54b4666
Robert Snyder <r.snyder49@gmail.com>
2018-08-10 21:56:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 
 def my_collect(array)
   i = 0 
+  new_array = []
   while i < array.length 
-    yield(array[i])
+    new_array.push(yield(array[i]))
     i += 1 
   end
+  return new_array
 end 
